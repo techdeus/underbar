07ff3020d58ca280aa6338f341459e950149c29c
techdeus <sullivan.marlon@gmail.com>
2018-08-27 22:55:59 -0700

    every and some is complete

--- src/underbar.js
+++ src/underbar.js
@@ -205,12 +205,23 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(wasFound, item) {
+      if(!wasFound) {
+        return false;
+      }
+      return iterator === undefined ? item : !!iterator(item);  
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    return _.reduce(collection, function(wasFound, item) {
+      if (wasFound) {
+        return true;
+      }
+      return iterator === undefined ? item : !!iterator(item);
+    }, false);
   };
 
 
